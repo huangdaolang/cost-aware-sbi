1900d7f206765add424930a1c3df5de093d8decd
Daolang Huang <daolang.huang@aalto.fi>
2024-04-19 01:43:51 +0300

    update temporal sir

--- simulators/temporal_sir.py
+++ simulators/temporal_sir.py
@@ -1,51 +1,76 @@
 import torch
 import time
+from sbi.utils.torchutils import *
 
 
-def simSIR(beta, gamma, device):
-    N = 5000  # population size
-    I = torch.tensor(1.0, device=device)  # infected individuals
-    S = torch.tensor(N - 1.0, device=device)  # susceptible individuals
-    t = torch.tensor(0.0, device=device)  # time
-    times = [t.item()]
-    types = [1]  # 1 for infection, 2 for removal
+class TemporalSIR(object):
+    def __init__(self,
+                 name="temporal_sir",
+                 population=1000,
+                 prior_lower=None,
+                 prior_upper=None,
+                 theta_dim=2,
+                 x_dim=12,
+                 num_bins=10):
 
-    while I > 0:
-        rate = (beta / N) * I * S + gamma * I
-        t += torch.distributions.Exponential(1 / rate).sample()
-        times.append(t.item())
+        if prior_lower is None:
+            prior_lower = [0.1, 0.1]
+        if prior_upper is None:
+            prior_upper = [1.0, 1.0]
 
-        if torch.rand(1, device=device) < (beta * S) / ((beta * S) + N * gamma):
-            I += 1
-            S -= 1
-            types.append(1)
-        else:
-            I -= 1
-            types.append(2)
+        self.name = name
+        self.N = population
+        self.theta_dim = theta_dim
+        self.x_dim = x_dim
+        self.num_bins = num_bins
+        self.prior = Uniform(torch.tensor(prior_lower), torch.tensor(prior_upper))
+                      # Uniform(prior_lower[1] * torch.ones(1), prior_upper[1] * torch.ones(1))]
 
-    removal_times = [times[i] - min([times[j] for j in range(len(types)) if types[j] == 2]) for i in range(len(times))
-                     if types[i] == 2]
-    final_size = N - S.item()
-    T = times[-1]
-    # print(removal_times)
+    def __call__(self, thetas):
+        beta = thetas[0]
+        gamma = thetas[1]
 
-    return {'removal_times': torch.tensor(removal_times, device=device),
-            'final_size': torch.tensor(final_size, device=device),
-            'T': torch.tensor(T, device=device)}
+        I = torch.tensor(1.0)  # infected individuals
+        S = torch.tensor(self.N - 1.0)  # susceptible individuals
+        t = torch.tensor(0.0)  # time
+        times = [t.item()]
+        types = [1]  # 1 for infection, 2 for removal
+
+        while I > 0:
+            rate = (beta / self.N) * I * S + gamma * I
+            t += torch.distributions.Exponential(1 / rate).sample()
+            times.append(t.item())
+
+            if torch.rand(1) < (beta * S) / ((beta * S) + self.N * gamma):
+                I += 1
+                S -= 1
+                types.append(1)
+            else:
+                I -= 1
+                types.append(2)
+
+        # removal_times = [times[i] - min([times[j] for j in range(len(types)) if types[j] == 2]) for i in
+        #                  range(len(times))
+        #                  if types[i] == 2]
+        T = times[-1]
+        removal_times = torch.tensor([times[i] for i in range(len(times)) if types[i] == 2])
+
+        removals_per_bin = torch.histc(removal_times, bins=self.num_bins, min=0, max=T)
+        final_size = self.N - S.item()
+
+        x = torch.cat([removals_per_bin, torch.tensor([final_size]), torch.tensor([T])])
+
+        return x
 
 
 if __name__ == "__main__":
+    simulator = TemporalSIR()
     N = 10
     # beta = torch.linspace(0.01, 0.5, N)
-    beta = 1
-    # gamma = 0.05
-    gamma = torch.linspace(0.01, 0.5, N)
-    times = torch.zeros(N)
-    for i in range(N):
-        st = time.time()
-        for _ in range(10):
-            result = simSIR(beta, gamma[i], device=torch.device('cpu'))
-        et = time.time()
-        times[i] = (et - st) / 100
+    beta = 0.9
+    gamma = 0.5
+    # gamma = torch.linspace(0.01, 0.5, N)
 
-    print(times)
+    result = simulator([beta, gamma])
+
+
