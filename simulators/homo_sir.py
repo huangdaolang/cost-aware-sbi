8fdef5b6fc2621f125250199730321d2cacc4b9f
Daolang Huang <daolang.huang@aalto.fi>
2024-04-18 23:54:48 +0300

    change to class

--- simulators/homo_sir.py
+++ simulators/homo_sir.py
@@ -1,30 +1,45 @@
 import torch
+from sbi.utils.torchutils import *
 
 
-def homoSIR(l, k, device=torch.device('cpu')):
-    N = 1000
-    S = N - 1
-    y = 1
-    while y > 0:
-        y -= 1
-        if k == 0:
-            I = torch.tensor(1.0, device=device)
-        else:
-            I = torch.distributions.Gamma(k, k).sample().to(device)
-        Z = torch.poisson(l * I).int()  # number of infectious contacts
+class HomoSIR(object):
+    def __init__(self, name="homo_sir",
+                 k=1,
+                 prior_lower=1,
+                 prior_upper=10.0,
+                 theta_dim=1,
+                 x_dim=1):
+        self.name = name
+        self.k = k
+        self.N = 1000
+        self.theta_dim = theta_dim
+        self.x_dim = x_dim
+        self.prior = Uniform(prior_lower * torch.ones(1), prior_upper * torch.ones(1))
 
-        if Z > 0:
-            for _ in range(Z):
-                u = torch.rand(1, device=device)
-                if u < (S / N):
-                    S -= 1
-                    y += 1
+    def __call__(self, thetas):
+        l = thetas[0]
+        S = self.N - 1
+        y = 1
+        while y > 0:
+            y -= 1
+            if self.k == 0:
+                I = torch.tensor(1.0)
+            else:
+                I = torch.distributions.Gamma(self.k, self.k).sample()
+            Z = torch.poisson(l * I).int()  # number of infectious contacts
 
-    return N - S
+            if Z > 0:
+                for _ in range(Z):
+                    u = torch.rand(1)
+                    if u < (S / self.N):
+                        S -= 1
+                        y += 1
+
+        return self.N - S
 
 
 if __name__ == "__main__":
     l = 3
     k = 1
-    print(homoSIR(l, k, device=torch.device('cpu')))
+    print(HomoSIR(1))
 
