294100cc6d0319dc0a5c544407496edafb723429
Daolang Huang <daolang.huang@aalto.fi>
2024-06-10 17:03:34 +0300

    add turin model

---
+++ run_turin.py
@@ -0,0 +1,82 @@
+import torch
+from cost_aware_snle_a import CostAwareSNLE_A
+from cost_aware_snpe_c import CostAwareSNPE_C
+from sbi.inference.snpe.snpe_c import SNPE_C
+from sbi.inference.snle.snle_a import SNLE_A
+from sbi.utils.torchutils import *
+from sbi.utils import process_prior
+import gpytorch
+
+import time
+import numpy as np
+import hydra
+from hydra.utils import get_original_cwd
+import utils
+import os
+
+
+@hydra.main(version_base=None, config_path="./configs", config_name="train")
+def train(cfg):
+    if cfg.device == "cuda":
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
+    simulator = hydra.utils.instantiate(cfg.simulator)
+    prior = simulator.prior
+    prior, *_ = process_prior(prior)
+
+    obs_x = simulator.x_obs
+    obs_theta = simulator.theta_true
+
+    x_npe, theta_npe = simulator.load_npe_data()
+
+    if cfg.simulator.inference == "npe":
+        inference = SNPE_C(prior=prior)
+    elif cfg.simulator.inference == "nle":
+        inference = SNLE_A(prior=prior)
+    else:
+        raise ValueError("Invalid inference method")
+
+    density_estimator = inference.append_simulations(theta_npe, x_npe).train()
+    posterior = inference.build_posterior(density_estimator)
+
+    posterior_samples = posterior.sample((1000,), x=obs_x)
+
+    # CA-NPE
+    x_canpe, theta_canpe, w_canpe = simulator.load_ca_npe_data(cfg.mixture, cfg.k)
+
+    if cfg.simulator.inference == "npe":
+        inference_canpe = CostAwareSNPE_C(prior=prior)
+    elif cfg.simulator.inference == "nle":
+        inference_canpe = CostAwareSNLE_A(prior=prior)
+    else:
+        raise ValueError("Invalid inference method")
+
+    density_estimator_canpe = inference_canpe.append_simulations(theta_canpe, x_canpe).append_weights(w_canpe).train()
+    posterior_canpe = inference_canpe.build_posterior(density_estimator_canpe)
+
+    posterior_samples_canpe = posterior_canpe.sample((1000,), x=obs_x)
+
+    if cfg.checkpoint:
+        ckpt = {
+            "posterior_npe": posterior,
+            "posterior_samples_npe": posterior_samples,
+            "posterior_canpe": posterior_canpe,
+            "posterior_samples_canpe": posterior_samples_canpe,
+            "true_theta": obs_theta,
+        }
+
+        output_directory = get_original_cwd()
+        print(f"{output_directory=}")
+        if cfg.mixture is True:
+            checkpoint_path = os.path.join(output_directory, "sims", simulator.name, "mixture", simulator.inference, str(cfg.seed))
+        else:
+            checkpoint_path = os.path.join(output_directory, "sims", simulator.name, str(cfg.k), simulator.inference, str(cfg.seed))
+        if not os.path.exists(checkpoint_path):
+            os.makedirs(checkpoint_path)
+
+        checkpoint_path = os.path.join(checkpoint_path, "ckpt.tar")
+        torch.save(ckpt, checkpoint_path)
+
+
+if __name__ == "__main__":
+    train()
---
+++ simulators/turin.py
@@ -0,0 +1,71 @@
+import torch
+from sbi.utils.torchutils import *
+import numpy as np
+import os
+
+
+class Turin(object):
+    def __init__(self, name="turin",
+                 k=1,
+                 theta_1_lower=1e-9,
+                 theta_1_upper=1e-8,
+                 theta_2_lower=1e-9,
+                 theta_2_upper=1e-8,
+                 theta_3_lower=1e9,
+                 theta_3_upper=1e10,
+                 theta_4_lower=1e-10,
+                 theta_4_upper=1e-9,
+                 theta_dim=4,
+                 x_dim=801,
+                 inference=None):
+        self.path = os.path.dirname(os.path.realpath(__file__))
+        self.name = name
+        self.k = k
+        self.theta_dim = theta_dim
+        self.x_dim = x_dim
+        self.prior = [Uniform(torch.tensor([theta_1_lower]), torch.tensor([theta_1_upper])),
+                      Uniform(torch.tensor([theta_2_lower]), torch.tensor([theta_2_upper])),
+                      Uniform(torch.tensor([theta_3_lower]), torch.tensor([theta_3_upper])),
+                      Uniform(torch.tensor([theta_4_lower]), torch.tensor([theta_4_upper]))]
+
+        self.theta_true = torch.tensor([4e-9, 7e-9, 5e9, 3e-10])
+        self.x_obs = torch.tensor(np.load(f'{self.path}/../data/turin_x_obs.npy'))
+
+        self.cost_alpha = torch.tensor(np.load(f'{self.path}/../data/turin_cost_alpha.npy'))
+        self.cost_beta = torch.tensor(np.load(f'{self.path}/../data/turin_cost_beta.npy'))
+
+    def sample_theta(self, size):
+        return NotImplementedError
+
+    def __call__(self, thetas):
+        return NotImplementedError
+
+    def load_npe_data(self):
+        x = torch.tensor(np.load(f'{self.path}/../data/turin_x_nle.npy'))
+        theta = torch.tensor(np.load(f'{self.path}/../data/turin_theta_nle.npy'))
+
+        return x, theta
+
+    def load_ca_npe_data(self, mixture=True, k=2):
+        if mixture:
+            x = torch.tensor(np.load(f'{self.path}/../data/turin_x_ca_nle_mixture.npy'))
+            theta = torch.tensor(np.load(f'{self.path}/../data/turin_theta_ca_nle_mixture.npy'))
+            w = torch.tensor(np.load(f'{self.path}/../data/turin_weights_mixture.npy'))
+            w = w / w.sum()
+        else:
+            x = torch.tensor(np.load(f'{self.path}/../data/turin_x_ca_nle_k2.npy'))
+            theta = torch.tensor(np.load(f'{self.path}/../data/turin_theta_ca_nle_k2.npy'))
+            w = self.cost_function(theta[:, 2], k)
+            w = w / w.sum()
+
+        return x, theta, w
+
+    def cost_function(self, theta, k):
+        return (self.cost_alpha * theta + self.cost_beta)**k
+
+
+if __name__ == "__main__":
+    turin = Turin()
+    turin.load_ca_npe_data(False)
+
+
